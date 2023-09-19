ba8c0f4e236ad2eac76ca401c5610aa491660431
JackyLuCN <j089325055@gmail.com>
2023-09-19 14:09:11 +0800

    week3

---
+++ Week3class.R
@@ -0,0 +1,9 @@
+students<-read.table('https://www.dipintothereef.com/uploads/3/7/3/5/37359245/students.txt',header=T, sep="\t", dec='.') # read data set from url
+str(students)
+rm(list=ls()) # clean memory
+library (datasets) # load package
+data(iris) # import dataset
+head (iris) # visualize 'head' dataset
+Order<-order(iris$Sepal.Width,decreasing =T)
+Order
+iris[Order,]
